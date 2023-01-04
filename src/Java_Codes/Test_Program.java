0458ee3ebe2516abf1b7b7e739dca44ee5255315
Shubham123Raj <shubham123raj4488@gmail.com>
2023-01-04 12:39:14 +0530

    First Commit

---
+++ src/Java_Codes/Test_Program.java
@@ -0,0 +1,10 @@
+package Java_Codes;
+
+public class Test_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My First Program to Check out and push it to github repository");
+	}
+
+}
